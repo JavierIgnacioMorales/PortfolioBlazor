c61c92e7a1b38ba465835aab9ddc24dfb1b89033
Javier Morales <javier.morales@educacion.gob.ar>
2024-08-07 12:58:53 -0300

    creando idea y estructura inicial

---
+++ wwwroot/site.js
@@ -0,0 +1,27 @@
+﻿
+
+
+
+window.toggleImage = function (isVisible) {
+    var image = document.querySelector('.imgaboutme');
+    var text = document.querySelector('.textaboutme');
+    if (isVisible) {
+        image.style.width = '100%';
+        text.style.width = '25%';
+    } else {
+        image.style.width = '0%';
+        text.style.width = '100%';
+    }
+};
+
+window.toggleProyects = function (isVisible) {
+    var proyects = document.querySelector('.proyectos');
+    var image = document.querySelector('.imgproyects');
+    if (isVisible) {
+        proyects.style.width = '100%';
+        image.style.width = '0%';
+    } else {
+        proyects.style.width = '0%';
+        image.style.width = '100%';
+    }
+}
