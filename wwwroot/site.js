4ee79a2abdc2f47dfa15f938af6c90dc8a8ba023
Javier Morales <javier.morales@educacion.gob.ar>
2024-08-09 13:02:10 -0300

    UN POCO DE RESPONSIVIDAD

--- wwwroot/site.js
+++ wwwroot/site.js
@@ -5,12 +5,27 @@
 window.toggleAboutme = function (isVisible) {
     var image = document.querySelector('.imgaboutme');
     var text = document.querySelector('.conttext');
+    var textvert = document.querySelector('.conttextvertical');
     if (isVisible) {
         image.style.width = '100%';
         text.style.width = '20%';
+        textvert.style.height = '20%';
     } else {
         image.style.width = '0%';
         text.style.width = '100%';
+        textvert.style.height = '100%';
+    }
+};
+
+window.toggleAboutmevert = function (isVisible) {
+    var image = document.querySelector('.imgaboutme');
+    var text = document.querySelector('.conttextvertical');
+    if (isVisible) {
+        image.style.height = '100%';
+        text.style.height = '20%';
+    } else {
+        image.style.height = '0%';
+        text.style.height = '100%';
     }
 };
 
@@ -26,15 +41,18 @@ window.toggleProyects = function (isVisible) {
 }
 
 window.toggleTecnology = function (isVisible) {
-    var text = document.querySelector('.texttecnology');
+    var text = document.querySelector('.conttexttec');
+    var textint = document.querySelector('.texttecnology')
     var conttec = document.querySelector('.alltecnology');
     if (isVisible) {
         conttec.style.left = '0';
         text.style.left = '80%';
         text.style.width = '20%';
+        textint.style.left = '80%';
     } else {
         conttec.style.left = '-80%';
         text.style.left = '0';
-        text.style.width = '40%';
+        text.style.width = '30%';
+        textint.style.left = '0%';
     }
 };
